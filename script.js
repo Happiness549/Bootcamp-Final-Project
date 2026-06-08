288cd24a7e5b2b0f84349b7d76ea61a29395eff0
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-06-08 16:46:27 +0200

    Add script.js file to make the project reactive when buttons are clicked

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const emailInput = document.getElementById("email-input");
+const form = document.getElementById("form");
+const errorMessage = document.getElementById("error-message");
+const successCard = document.getElementById("success-card");
+const formContainer = document.getElementById("form-container");
+const dismissBtn = document.getElementById("dismissBtn");
+
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+form.addEventListener("submit", function (event) {
+event.preventDefault();
+
+const emailValue = emailInput.value.trim();
+errorMessage.textContent = "";
+
+if (emailValue === "") {
+    errorMessage.textContent = "Valid email required";
+    return;
+}
+
+if (!emailPattern.test(emailValue)) {
+    errorMessage.textContent = "Enter a valid email address";
+    return;
+}
+
+formContainer.style.display = "none";
+successCard.style.display = "block";
+console.log("successCard element:", successCard);
+console.log("Exactly");
+
+
+});
+
+dismissBtn.addEventListener("click", function () {
+errorMessage.textContent = "";
+
+successCard.style.display = "none";
+formContainer.style.display = "block";
+form.reset();
+
+
+});
